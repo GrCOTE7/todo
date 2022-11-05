815bb81387dee72c1f1b5c7a80b84fcf4e502993
Lionel COTE <grcote7@gmail.com>
2022-11-05 15:59:57 +0100

    Fix: IdiORM charset UTF-8 trouble

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use ORM;
+use PDO;
 
 abstract class Model
 {
     public function __construct()
     {
         ORM::configure('mysql:host='. APP['database']['host'].';dbname=' . APP['database']['dbname']);
+        ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
         ORM::configure('username', APP['database']['user']);
         ORM::configure('password', APP['database']['password']);
     }
