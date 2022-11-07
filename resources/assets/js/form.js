7d8e6addb0707a6d115c2e58fae9cdc8918bda1b
Lionel COTE <grcote7@gmail.com>
2022-11-07 22:08:26 +0100

    Feat: Factorise Listener on name and content

--- resources/assets/js/form.js
+++ resources/assets/js/form.js
@@ -1,65 +1,24 @@
-
-
-
 let status = document.getElementsByClassName('status')
 let nameElt = document.getElementById('name')
 let contentElt = document.getElementById('content')
 let modifBtn = document.getElementById('btnDelModif')
 
-let initialNameValue = nameElt.value
-let initialContentValue = contentElt.value
+let init=[];
+init['nameElt'] = nameElt.value
+init['contentElt'] = contentElt.value
 
-const watchingElts = [ 'name', 'content']
+const watchingElts = [nameElt, contentElt]
 
-watchingElts.forEach(elt=> {
-  console.log(${elt+'Elt'})
-  // item.addEventListener('input', (e) => {
-  //   console.log(`${item} is this value: ${e.target.value}`);
-  //   item.nextElementSibling.textContent = e.target.value;
-  // });
-})
-
-// console.log(watchingElts)
-// watchingElts.forEach((elt, init) => {
-//   console.log(123)
-
-for (const k in watchingElts) {
-  // console.log(`${property}: ${watchingElts[property]}`);
-  // console.log(`${watchingElts[k]+'Elt'}`);
-}
-// let v = ''
-// elt.addEventListener('input', function (evt) {
-//   newEltValue = elt.value;
-//   if (newEltValue != initialNameValue) {
-//     modifBtn.classList.remove("hidden")
-//   }
-//   else {
-//     modifBtn.classList.add("hidden")
-//   }
-// })
-
-// })
-// console.log(nameElt);
-let newNameValue = ''
-nameElt.addEventListener('input', function (e) {
-  newNameValue = nameElt.value;
-  if (newNameValue != initialNameValue) {
-    modifBtn.classList.remove("hidden")
-  }
-  else {
-    modifBtn.classList.add("hidden")
-  }
-})
-
-let newContentValue = ''
-contentElt.addEventListener('input', function (e) {
-  newContentValue = contentElt.value;
-  if (newContentValue != initialContentValue) {
-    modifBtn.classList.remove("hidden")
-  }
-  else {
-    modifBtn.classList.add("hidden")
-  }
+watchingElts.forEach(elt => {
+  elt.addEventListener('input', function (e) {
+    let newValue = elt.value;
+    if (newValue != init[elt.name+'Elt']) {
+      modifBtn.classList.remove("hidden")
+    }
+    else {
+      modifBtn.classList.add("hidden")
+    }
+  })
 })
 
 document.getElementById('btnDelModif').addEventListener('click', () => {
