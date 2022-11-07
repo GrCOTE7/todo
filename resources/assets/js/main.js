79bb347de9a746db685484bfa98f4385827cea8f
Lionel COTE <grcote7@gmail.com>
2022-11-07 10:35:59 +0100

    Feat: Modif btn dynamic

--- resources/assets/js/main.js
+++ resources/assets/js/main.js
@@ -1,8 +1,37 @@
 
+
 let status = document.getElementsByClassName('status')
+let nameElt = document.getElementById('name');
+
+let initialValue = nameElt.value;
 
 
-console.log(status)
-// status.addEventListener('click', ()=>{
-//   console.log('clicked')
-// })
+
+console.log(initialValue)
+// console.log(nameValue)
+// console.log(name.value)
+
+let newValue='';
+
+nameElt.addEventListener('input', function (e) {
+  newValue = nameElt.value;
+  if (newValue != initialValue){
+    document.getElementById('btnDelModif').classList.remove("hidden") 
+    console.log(initialValue)
+  }
+  else {
+    document.getElementById('btnDelModif').classList.add("hidden") 
+      }
+  
+  
+})
+
+
+
+window.load = (() => {
+  console.log(status)
+  // status.addEventListener('click', ()=>{
+  //   console.log('clicked')
+  // })
+
+})
