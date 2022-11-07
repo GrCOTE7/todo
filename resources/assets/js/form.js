f88e8e183094b9ab51267a8157088584b5203cb4
Lionel COTE <grcote7@gmail.com>
2022-11-07 21:14:35 +0100

    Fixg: factorisation

--- resources/assets/js/form.js
+++ resources/assets/js/form.js
@@ -9,6 +9,37 @@ let modifBtn = document.getElementById('btnDelModif')
 let initialNameValue = nameElt.value
 let initialContentValue = contentElt.value
 
+const watchingElts = [ 'name', 'content']
+
+watchingElts.forEach(elt=> {
+  console.log(${elt+'Elt'})
+  // item.addEventListener('input', (e) => {
+  //   console.log(`${item} is this value: ${e.target.value}`);
+  //   item.nextElementSibling.textContent = e.target.value;
+  // });
+})
+
+// console.log(watchingElts)
+// watchingElts.forEach((elt, init) => {
+//   console.log(123)
+
+for (const k in watchingElts) {
+  // console.log(`${property}: ${watchingElts[property]}`);
+  // console.log(`${watchingElts[k]+'Elt'}`);
+}
+// let v = ''
+// elt.addEventListener('input', function (evt) {
+//   newEltValue = elt.value;
+//   if (newEltValue != initialNameValue) {
+//     modifBtn.classList.remove("hidden")
+//   }
+//   else {
+//     modifBtn.classList.add("hidden")
+//   }
+// })
+
+// })
+// console.log(nameElt);
 let newNameValue = ''
 nameElt.addEventListener('input', function (e) {
   newNameValue = nameElt.value;
