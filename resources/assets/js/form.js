143e718b99f470b474c19a1149f2f04ee0fe7337
Lionel COTE <grcote7@gmail.com>
2022-11-07 22:18:16 +0100

    Feat: Simplifacation Dynamic listener

--- resources/assets/js/form.js
+++ resources/assets/js/form.js
@@ -1,18 +1,16 @@
-let status = document.getElementsByClassName('status')
+// let status = document.getElementsByClassName('status')
 let nameElt = document.getElementById('name')
 let contentElt = document.getElementById('content')
 let modifBtn = document.getElementById('btnDelModif')
-
 let init=[];
-init['nameElt'] = nameElt.value
-init['contentElt'] = contentElt.value
 
 const watchingElts = [nameElt, contentElt]
 
 watchingElts.forEach(elt => {
+  init[elt] = elt.value
   elt.addEventListener('input', function (e) {
     let newValue = elt.value;
-    if (newValue != init[elt.name+'Elt']) {
+    if (newValue != init[elt]) {
       modifBtn.classList.remove("hidden")
     }
     else {
